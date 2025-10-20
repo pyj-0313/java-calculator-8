008baa768f8188499e78ff7fc4919ef8206a593a
pyj-0313 <pyjun0313@gmail.com>
2025-10-20 17:25:38 +0900

    refactor: Application 상수화 및 유틸 클래스화

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
-public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+import camp.nextstep.edu.missionutils.Console;
+
+public final class Application {
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String RESULT_MESSAGE = "결과 : ";
+
+    private Application() { }
+
+    public static void main(final String[] args) {
+        System.out.println(INPUT_MESSAGE);
+        final String input = Console.readLine();
+        final int result = StringCalculator.add(input);
+        System.out.println(RESULT_MESSAGE + result);
     }
 }
